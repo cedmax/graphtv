b0ac9927fd9b08e0f504713ab650479c5539335e
Marco Cedaro <cedmax@users.noreply.github.com>
2020-04-01 18:52:03 +0100

    small tweaks

--- modules/api.js
+++ modules/api.js
@@ -1,30 +1,36 @@
-const trakt = require('./trakt')
-const extended = 'full'
+const trakt = require("./trakt");
+const extended = "full";
 const showSelector = ({
-  show: { 
-    title, 
-    year, 
-    ids: { 
-      trakt: id 
-    }
-  }
-}) => ({title, year, id})
+  show: {
+    title,
+    year,
+    ids: { trakt: id },
+  },
+}) => ({ title: title.replace("&amp;", "&"), year, id });
 
 module.exports = {
-  searchShow: async (query) => {
-    const results = await trakt.search.text({ query, type: 'show'})
-    return results.map(showSelector)
+  searchShow: async query => {
+    const results = await trakt.search.text({ query, type: "show" });
+    return results.map(showSelector);
   },
-  getSeasons: async (id) => {
-    const complete = await trakt.seasons.summary({ id, id_type:'trakt', extended })
-    const filteredSeasons = complete.filter(({number}) => !!number)
+  getSeasons: async id => {
+    const complete = await trakt.seasons.summary({
+      id,
+      id_type: "trakt",
+      extended,
+    });
+    const filteredSeasons = complete.filter(({ number }) => !!number);
 
-    const fetchSeasons = filteredSeasons.map(async (season) => { 
-      const episodes = await trakt.seasons.season({ id, season: season.number, extended })
-      return { ...season, episodes }
-    })
-  
-    const seasons = await Promise.all(fetchSeasons)
-    return seasons
+    const fetchSeasons = filteredSeasons.map(async season => {
+      const episodes = await trakt.seasons.season({
+        id,
+        season: season.number,
+        extended,
+      });
+      return { ...season, episodes };
+    });
+
+    const seasons = await Promise.all(fetchSeasons);
+    return seasons;
   },
-}
+};
--- src/Components/ShowAutocomplete.js
+++ src/Components/ShowAutocomplete.js
@@ -1,55 +1,58 @@
-import React, { Component } from 'react';
-import Autosuggest from 'react-autosuggest';
-import axios from 'axios';
+import React, { Component } from "react";
+import Autosuggest from "react-autosuggest";
+import axios from "axios";
 
-const formatItem = (item) => `${item.title} - ${item.year}`
+const formatItem = item => `${item.title} - ${item.year}`;
 
 class App extends Component {
-  fetchList = async (query) => {
-    const { data } = await axios.get(`/api/search/${query}`);
-    this.setState({ list: data })
-  }
-  
-  fetchShow = async ({id}) => {
-    this.props.onLoading(this.state.value)
+  fetchList = async query => {
+    const { data } = await axios.get(`/api/search/${query.toLowerCase()}`);
+    this.setState({ list: data });
+  };
+
+  fetchShow = async ({ id }) => {
+    this.props.onLoading(this.state.value);
     const { data } = await axios.get(`/api/show/${id}`);
-    this.props.onChange(data)
-  }
+    this.props.onChange(data);
+  };
 
   state = {
-    value: '',
-    list: []
-  }
+    value: "",
+    list: [],
+  };
 
-  requestTimer = null
+  requestTimer = null;
 
   render() {
     const { id } = this.props;
-    const {value, list} = this.state;
+    const { value, list } = this.state;
 
     return (
       <Autosuggest
-        inputProps={{ 
-          id, 
-          value, 
-          placeholder: 'Breaking Bad', 
-          onChange: (e) => this.setState({ value: e.target.value }) 
+        inputProps={{
+          id,
+          value,
+          placeholder: "Breaking Bad",
+          onChange: e => this.setState({ value: e.target.value }),
         }}
         suggestions={list}
-        onSuggestionsClearRequested={() => this.setState({list: []})}
-        getSuggestionValue={(item) => item.title}
+        onSuggestionsClearRequested={() => this.setState({ list: [] })}
+        getSuggestionValue={item => item.title}
         onSuggestionSelected={(event, { suggestion }) => {
-          this.setState({ value: formatItem(suggestion), list: [ suggestion ] }, () => this.fetchShow(suggestion))
+          this.setState(
+            { value: formatItem(suggestion), list: [suggestion] },
+            () => this.fetchShow(suggestion)
+          );
         }}
-        onSuggestionsFetchRequested={({value}) => {
-          clearTimeout(this.requestTimer)
+        onSuggestionsFetchRequested={({ value }) => {
+          clearTimeout(this.requestTimer);
           if (value) {
-            this.requestTimer = setTimeout(() => this.fetchList(value), 400)
+            this.requestTimer = setTimeout(() => this.fetchList(value), 400);
           }
         }}
-        renderSuggestion={(item) => formatItem(item)}
+        renderSuggestion={item => formatItem(item)}
       />
-    )
+    );
   }
 }
 
